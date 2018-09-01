6a2d5a96ef9f046e74d99b0bf49fd6f514432362
cballam <cole.ballam@gmail.com>
2018-09-01 11:33:34 +1000

    Added 'hello world' style Django app
    Website directory holds the basic Django app. To run it, have up to date repo clone, cd into website and enter 'python manage.py runserver' in command prompt/console

---
+++ website/basesite/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'base.html') #IMPORTANT NOTE - 'base.html' is
+    #automatically found, as django takes all templates from (project)/(app)/templates
+    #and considers them the same thing. may make things difficult if we have multiple apps
+
+    # Also for dynamically rendered pages pass a dictionary into render, and
+    # this will be used in Jinja templating
