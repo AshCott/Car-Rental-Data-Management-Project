5e27f5745e8025bb26045117b940a2f77105e7a0
cballam <cole.ballam@gmail.com>
2018-09-03 11:53:39 +1000

    Minor template and structure changes
    Now has search.html, which extends base.html template. Clicking 'Search Available Cars' link directs user to the search page

--- website/basesite/urls.py
+++ website/basesite/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
- url('', views.index, name='index') #base url
+ url(r'^$', views.index, name='index'), #base url
+ url('search/', views.search, name='search')
 ]
--- website/basesite/views.py
+++ website/basesite/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'base.html') #IMPORTANT NOTE - 'base.html' is
+    return render(request, 'basesite/base.html') #IMPORTANT NOTE - 'base.html' is
     #automatically found, as django takes all templates from (project)/(app)/templates
     #and considers them the same thing. may make things difficult if we have multiple apps
 
     # Also for dynamically rendered pages pass a dictionary into render, and
     # this will be used in Jinja templating
+
+def search(request):
+    return render(request, 'basesite/search.html')
