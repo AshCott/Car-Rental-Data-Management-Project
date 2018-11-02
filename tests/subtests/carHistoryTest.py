2fec3d010010b3378e3f188e0332a5f5ace8d42e
CtrlAltDeIicious <smithy260@rocketmail.com>
2018-11-02 15:43:50 +1000

    added comments to clarify code

--- tests/subtests/carHistoryTest.py
+++ tests/subtests/carHistoryTest.py
@@ -6,36 +6,52 @@ def main(driver, url):
     # Get the home page of the site
     driver.get(url)
 
+    # Finding login buton
     logIn = driver.find_element_by_id('login')
     sleep(1)
+    # Clicking Login button
     logIn.click()
     sleep(1)
 
-
+    # Finding the Username field on login screen
     usernameField = driver.find_element_by_id("id_username")
+    # Entering the username "admin"
     usernameField.send_keys("admin")
     sleep(1)
+    # Finding the password field on login screen
     usernameField = driver.find_element_by_id("id_password")
     usernameField.send_keys("admin")
     sleep(1)
+    # Locating the submit button to log in.
     login = driver.find_element_by_id('submit_btn')
+    # Clicking button
     login.click()
     sleep(3)
 
-    # First test: Click search button
-
+    # Going back to homepage after logging in 
+    # Finding home button and clicking
     home = driver.find_element_by_name('Home')
     home.click()
     sleep(1)
 
-    #assert 'Car Rental Company' in driver.title
+    # Using one of the featured cars on the home page
+    # to go to individual results page
     featuredCar1 = driver.find_element_by_name('featureCar1')
     featuredCar1.click()
     sleep(1)
 
-    historyOpen = driver.find_element_by_id('history')
-    historyOpen.click()
-    sleep(1)
+    # Attempt to open car rental history panel
+      try:
+        historyOpen = driver.find_element_by_id('history')
+        historyOpen.click()
+        sleep(1)
+    except Exception:
+        print("FAILED")
+        print("\n Testing Halted")
+        exit()
+    print("PASSED")
+    
+   
 
     # clean up by logging out first
     logout = driver.find_element_by_id('logout_btn')
